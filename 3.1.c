5a3a338562497983b3f530b88ab64f31cf6522ee
khhjsrw <2871036467@qq.com>
2025-07-15 11:57:52 +0800

    test

--- 3.1.c
+++ 3.1.c
@@ -6,7 +6,7 @@ int main()
     if (lseek(STDIN_FILENO, 0, SEEK_CUR) == -1)
         printf("Cannot seek\n");
     else
-        printf("seek OK\n");
+        //printf("seek OK\n");
 
     return 0;
 }
